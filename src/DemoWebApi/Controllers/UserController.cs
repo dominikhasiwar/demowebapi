2c640d66d3e8a9c6de738c237af649f803bd01f4
Dominik Hasiwar <dominik.hasiwar@gmail.com>
2022-08-31 21:04:49 +0200

    Add controllers

---
+++ src/DemoWebApi/Controllers/UserController.cs
@@ -0,0 +1,55 @@
+﻿using DemoWebApi.Managers;
+using DemoWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebApi.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]    
+    public class UserController : ControllerBase
+    {
+        private readonly IUserManager _userManager;
+
+        public UserController(IUserManager userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        [MapToApiVersion("1.0")]
+        public async Task<UserModel[]> GetUsers()
+        {
+            return await _userManager.GetUsers();
+        }
+
+        [HttpGet("{userId}")]
+        [MapToApiVersion("1.0")]
+        public async Task<UserModel> GetUser(int userId)
+        {
+            return await _userManager.GetUser(userId);
+        }
+
+        [HttpPost]
+        [MapToApiVersion("1.0")]
+        public async Task<UserModel> CreateUser([FromBody] SaveUserModel model)
+        {
+            return await _userManager.CreateUser(model);
+        }
+
+        [HttpPut("{userId}")]
+        [MapToApiVersion("1.0")]
+        public async Task<UserModel> UpdateUser(int userId, [FromBody] SaveUserModel model)
+        {
+            return await _userManager.UpdateUser(userId, model);
+        }
+
+        [HttpDelete("{userId}")]
+        [MapToApiVersion("2.0")]
+        public void DeleteUser(int userId)
+        {
+            _userManager.DeleteUser(userId);
+        }
+    }
+}
