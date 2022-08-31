a9215d57eb8d8b7502f6e7a81a58753e37a8f13c
Dominik Hasiwar <dominik.hasiwar@gmail.com>
2022-08-31 21:04:35 +0200

    Add profiles

---
+++ src/DemoWebApi/Profiles/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using DemoWebApi.Entities;
+using DemoWebApi.Models;
+
+namespace DemoWebApi.Profiles
+{
+    public class UserProfile : Profile
+    {
+        public UserProfile()
+        {
+            CreateMap<User, UserModel>();
+
+            CreateMap<SaveUserModel, User>();
+        }
+    }
+}
