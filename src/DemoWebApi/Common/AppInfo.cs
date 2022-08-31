71bc831fa1ab04d907212edc69aa8b6dcbe11c3b
Dominik Hasiwar <dominik.hasiwar@gmail.com>
2022-08-31 21:04:14 +0200

    Add common logic

---
+++ src/DemoWebApi/Common/AppInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace DemoWebApi.Common
+{
+    public class AppInfo
+    {
+        public string AppName { get; } = "DemoWebApi";
+
+        public string AppVersion { get; } = "1.0.0";
+
+        public static AppInfo Current { get; } = new AppInfo();
+
+        private AppInfo()
+        {
+        }
+    }
+}
