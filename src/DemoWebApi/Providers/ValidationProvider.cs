33299d312b94ed9fce0e9c93691e090907e422b9
Dominik Hasiwar <dominik.hasiwar@gmail.com>
2022-08-31 21:05:12 +0200

    Add validation logic

---
+++ src/DemoWebApi/Providers/ValidationProvider.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+
+namespace DemoWebApi.Providers
+{
+    public interface IValidationProvider
+    {
+        IValidator[] GetValidators();
+    }
+
+    public class ValidationProvider : IValidationProvider
+    {
+        private readonly IValidator[] _validators;
+
+        public ValidationProvider(IValidator[] validators)
+        {
+            _validators = validators;
+        }
+
+        public IValidator[] GetValidators()
+        {
+            return _validators;
+        }
+    }
+}
---
+++ src/DemoWebApi/Validators/UserValidator.cs
@@ -0,0 +1,17 @@
+﻿using DemoWebApi.Models;
+using FluentValidation;
+
+namespace DemoWebApi.Validators
+{
+    public class UserValidator : AbstractValidator<SaveUserModel>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty();
+
+            RuleFor(x => x.LastName).NotEmpty();
+
+            RuleFor(x => x.UserName).NotEmpty();
+        }
+    }
+}
