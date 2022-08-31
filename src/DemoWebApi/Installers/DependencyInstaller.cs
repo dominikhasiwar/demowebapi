10cd35d4af859ffa69a924f4503f8d8c4d67225f
Dominik Hasiwar <dominik.hasiwar@gmail.com>
2022-08-31 21:05:37 +0200

    Add ioc logic

---
+++ src/DemoWebApi/Installers/DependencyInstaller.cs
@@ -0,0 +1,33 @@
+﻿using Castle.Facilities.AspNetCore;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using DemoWebApi.Managers;
+using DemoWebApi.Providers;
+using FluentValidation;
+
+namespace DemoWebApi.Installers
+{
+    public class DependencyInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(Component
+                .For<IUserManager>()
+                .ImplementedBy<UserManager>()
+                .LifestyleSingleton());
+
+            container.Register(Component
+                .For<IValidationProvider>()
+                .ImplementedBy<ValidationProvider>()
+                .LifestyleSingleton()
+                .CrossWired());
+
+            container.Register(Classes
+                .FromAssembly(typeof(DependencyInstaller).Assembly)
+                .BasedOn<IValidator>()
+                .WithServiceAllInterfaces()
+                .LifestyleSingleton());
+        }
+    }
+}
